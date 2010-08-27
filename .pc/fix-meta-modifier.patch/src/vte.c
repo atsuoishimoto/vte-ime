9c334785e2a68fd7b12e81095dffab6968804a40
Robert Ancell <robert.ancell@canonical.com>
2010-08-27 22:18:17 +1000

    [ Anders Kaseorg ] * debian/patches/fix-alt-modifier.patch:   - Replace with upstream reversion commit c56c3c4. * debian/patches/fix-meta-modifier.patch:   - Take upstream commit 724195b to really fix Meta.

--- .pc/fix-meta-modifier.patch/src/vte.c
+++ .pc/fix-meta-modifier.patch/src/vte.c
@@ -4910,13 +4910,8 @@ vte_terminal_read_modifiers (VteTerminal *terminal,
 	GdkModifierType modifiers;
 
 	/* Read the modifiers. */
-	if (gdk_event_get_state((GdkEvent*)event, &modifiers)) {
-		GdkKeymap *keymap;
-		keymap = gdk_keymap_get_for_display (
-				gdk_drawable_get_display (((GdkEventAny *)event)->window));
-		gdk_keymap_add_virtual_modifiers (keymap, &modifiers);
+	if (gdk_event_get_state((GdkEvent*)event, &modifiers))
 		terminal->pvt->modifiers = modifiers;
-	}
 }
 
 /* Read and handle a keypress event. */
